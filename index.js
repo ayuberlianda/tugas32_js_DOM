912b5422d5c223b0f688da56d57fb0708a9e339f
ayuberlianda <71421525+ayuberlianda@users.noreply.github.com>
2020-09-22 09:26:04 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function panggilInternal(){
+			var NamaDepan = document.getElementById("NamaDepan")
+			var NamaBelakang = document.getElementById("NamaBelakang")
+			var Hobi = document.getElementById("Hobi")
+			
+		console.log(NamaDepan)
+		console.log(NamaBelakang)
+		console.log(Hobi)
+	}
